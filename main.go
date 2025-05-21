0d7627b04dfb97cc30abc92e52a44835896a49a9
offlinebot <flintstonelg@gmail.com>
2025-05-22 00:51:21 +0200

    x

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"github.com/OfflineBot/gomat/matrix1"
 	"github.com/OfflineBot/gomat/matrix2"
 )
 
 func main() {
 	x := matrix2.NewMatrix2([][]float32 {
-		[]float32 {1.0, 2.0},
-		[]float32 {3.0, 4.0},
-		[]float32 {1.0, 2.0},
+		{1.0},
+		{2.0},
 	});
-	y := matrix1.NewMatrix1([]float32 {1.0, 1.0});
-	x.AddMatrix1(y)
-	x.Println()
+	
+	y := matrix2.NewMatrix2([][]float32 {
+		{1.0, 2.0},
+	})
+	z := x.Dot(y)
+	z.Println()
 }
